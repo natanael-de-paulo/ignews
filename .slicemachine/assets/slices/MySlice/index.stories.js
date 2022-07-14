57d86f787efe8da6486914450ab98c3f9e2555e5
Natanael <natanaeldepaulo123@gmail.com>
2022-07-14 15:48:10 -0300

    configurando prismic cms

---
+++ .slicemachine/assets/slices/MySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlice';
+
+export default {
+  title: 'slices/MySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_slice","items":[],"primary":{},"id":"_Default"}} />
+_Default.storyName = 'Default'
