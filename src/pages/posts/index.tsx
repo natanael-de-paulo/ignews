0b2d15d05d180279e99c5395a0b86e1a91467a19
Natanael <natanaeldepaulo123@gmail.com>
2022-07-15 23:42:32 -0300

    Estilizando e estruturando pagina de posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from './style.module.scss';
+import styles from './styles.module.scss';
 
 export default function Posts(){
   return (
@@ -7,14 +7,27 @@ export default function Posts(){
       <Head>
         <title>Posts | Ignews</title>
       </Head>
-      <main>
-        <div>
-          <a href="">
+      <main className={styles.container}>
+        <article className={styles.posts}>
+          <a href="#">
             <time>2022</time>
             <strong>Teste</strong>
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam dolore nostrum rem facilis. Quas, magnam expedita omnis enim ducimus, a at officiis laudantium adipisci eaque nisi, ut tempore atque. </p>
           </a>
-        </div>
+
+          <a href="#">
+            <time>2022</time>
+            <strong>Teste</strong>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam dolore nostrum rem facilis. Quas, magnam expedita omnis enim ducimus, a at officiis laudantium adipisci eaque nisi, ut tempore atque. </p>
+          </a>
+
+
+          <a href="#">
+            <time>2022</time>
+            <strong>Teste</strong>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam dolore nostrum rem facilis. Quas, magnam expedita omnis enim ducimus, a at officiis laudantium adipisci eaque nisi, ut tempore atque. </p>
+          </a>
+        </article>
       </main>
     </>
   )
