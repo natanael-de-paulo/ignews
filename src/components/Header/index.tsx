b1b86118ce9f228f65399da5859e60eca627a84e
Natanael <natanaeldepaulo123@gmail.com>
2022-07-19 23:15:39 -0300

    ActiveLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
-import Link from 'next/link'
 import { FaGithub } from 'react-icons/fa'
+import { ActiveLink } from '../ActiveLink'
 import { SignInBtn } from './SignInBtn'
 import styles from './styles.module.scss'
 
 export function Header() {
+  
   return (
     <header className={ styles.headerContainer }>
       <div className={ styles.headerContent }>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active} href="#">Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a> 
-          </Link>
+          </ActiveLink>
         </nav>
         <SignInBtn />
       </div>
